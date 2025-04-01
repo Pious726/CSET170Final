1e6bc6fec73ff24e7c7a994aa51574c04fe8b02e
Tyler M. <tjmcdowell726@gmail.com>
2025-04-01 09:57:41 -0400

    created basic python routes

--- main.py
+++ main.py
@@ -10,5 +10,33 @@ conn = engine.connect()
 def loadapp():
     return render_template('base.html')
 
+@app.route('/login.html')
+def getlogins():
+    return render_template('login.html')
+
+@app.route('/home.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/account.html', methods=["GET"])
+def getAccounts():
+    return render_template('account.html')
+
+@app.route('/account.html', methods=["POST"])
+def seeAccount():
+    return render_template('account.html')
+
+@app.route('/deposit.html')
+def deposit():
+    return render_template('deposit.html')
+
+@app.route('/transfer.html')
+def transfer():
+    return render_template('transfer.html')
+
+@app.route('/admin.html')
+def authorizeAccounts():
+    return render_template('admin.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
