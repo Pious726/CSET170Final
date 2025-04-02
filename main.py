cbbe4721b189087ce53e1bb7c09dcfcf0d0affa5
Tyler M. <tjmcdowell726@gmail.com>
2025-04-02 10:36:05 -0400

    my account page works perfectly

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ def signup():
 
 @app.route('/login.html', methods=["GET"])
 def getlogins():
+    conn.execute(text('update users set IsLoggedIn = 0 where IsLoggedIn = 1'))
+    conn.commit()
     return render_template('login.html')
 
 @app.route('/login.html', methods=["POST"])
@@ -62,7 +64,8 @@ def home():
 
 @app.route('/account.html')
 def seeAccount():
-    account = conn.execute("select * from users where IsLoggedIn = 1")
+    account = conn.execute(text("select * from users where IsLoggedIn = 1")).fetchone()
+    print(account)
     return render_template('account.html', account=account)
 
 @app.route('/deposit.html')
